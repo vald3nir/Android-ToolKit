7f716eff9d6fcb860bcd919de67d2ee1251c87ea
Valdenir de Sousa Severino <vald3nir@gmail.com>
2022-07-30 17:03:45 -0300

    validate user name

--- utils/src/main/java/com/vald3nir/utils/validations/UserValidations.kt
+++ utils/src/main/java/com/vald3nir/utils/validations/UserValidations.kt
@@ -2,8 +2,14 @@ package com.vald3nir.utils.validations
 
 import android.util.Patterns
 
-fun isEmailValid(username: String): Boolean {
-    return Patterns.EMAIL_ADDRESS.matcher(username).matches()
+fun isNameValid(username: String): Boolean {
+    return if (username.isNotBlank()) {
+        username.trim().split("").size >= 2
+    } else false
+}
+
+fun isEmailValid(email: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
 
 fun isPasswordValid(password: String): Boolean {
